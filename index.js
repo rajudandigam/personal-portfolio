e4f4cc107366d0cd042d866376ce907087405ec2
Raju Dandigam <raju0574@gmail.com>
2019-04-21 16:34:04 -0700

    feat: added logic to hide loading and update profile data in DOM

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-function onReady() {
-  alert("Ready");
+async function onReady() {
+  const portfolioElement = document.querySelector('#portfolio');
+  const loadingElem = portfolioElement.querySelector('#loading');
+  const mainElem = portfolioElement.querySelector('main');
+  const aboutElem = mainElem.querySelector('#about');
+
+  const getTextNode = (text) => document.createTextNode(text);
+
+  try {
+    const response = await fetch('./data/profile.json');
+    const profileData = await response.json();
+
+    aboutElem.querySelector('#profileName').appendChild(getTextNode(profileData.fullName));
+    aboutElem.querySelector('#title').appendChild(getTextNode(profileData.title));
+    aboutElem.querySelector('#subTitle').appendChild(getTextNode(profileData.subTitle));
+  }
+  finally {
+    mainElem.classList.remove('hide');
+    loadingElem.classList.add('hide');
+  }
+
 };
 
 document.addEventListener('DOMContentLoaded', onReady);
